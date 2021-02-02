1813c0920745b772efdaaeb051bacb5dc5359812
Semih Şahan <s.semih.sahan@hotmail.com>
2021-02-02 17:15:07 +0300

    Program.cs

--- ReCapProject/ConsoleUI/Program.cs
+++ ReCapProject/ConsoleUI/Program.cs
@@ -10,11 +10,18 @@ namespace ConsoleUI
 		{
 			ProductManager productManager = new ProductManager(new InMemoryProductDal());
 
-			foreach (var product in productManager.GetAll())
-			{
-				Console.WriteLine(product.Description);
-			}
+			var result=productManager.GetAll();
 
+			foreach (var item in result)
+			{
+				Console.Write("Id={0} ",item.Id);
+				Console.Write("BrandId={0} ",item.BrandId);
+				Console.Write("ColorId={0} ", item.ColorId);
+				Console.Write("DailyPrice={0} ", item.DailyPrice);
+				Console.Write("ModelYear={0} ", item.ModelYear);
+				Console.Write("Description={0} ", item.Description);
+				Console.WriteLine();
+			}
 		}
 	}
 }
