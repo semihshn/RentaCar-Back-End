1385889c0319446a205329d39cbb8d3e75179655
Semih Şahan <s.semih.sahan@hotmail.com>
2021-02-06 12:08:43 +0300

    ...

--- ReCapProject/ConsoleUI/Program.cs
+++ ReCapProject/ConsoleUI/Program.cs
@@ -3,6 +3,7 @@ using DataAccess.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using System;
+using System.Linq;
 
 namespace ConsoleUI
 {
@@ -13,7 +14,7 @@ namespace ConsoleUI
 
 
 			CarManager carManager = new CarManager(new EfCarDal());
-
+			//Description özelliği en az 2 karakter olmalı ve DailyPrice özelliği pozitif bir değer olmalı şartını test ettik
 			EfCarDal efCarDal = new EfCarDal();
 			efCarDal.Add(new Car()
 			{
@@ -25,6 +26,12 @@ namespace ConsoleUI
 				
 			});
 
+			//Saçma bir kayıt eklemiştim onu sildim
+			//ReCapContext reCapContext = new ReCapContext();
+			//reCapContext.Cars.Remove(reCapContext.Cars.SingleOrDefault(c => c.Id == 4));
+			//reCapContext.SaveChanges();
+
+			//Tüm araçları ekrana getirdik
 			Console.WriteLine("Tüm Araçlar");
 			foreach (var cars in carManager.GetAll())
 			{
@@ -37,6 +44,7 @@ namespace ConsoleUI
 				Console.WriteLine();
 			}
 			Console.WriteLine("--------------------");
+			//BrandId değeri 3 olan araçları getirdik
 			Console.WriteLine("BrandId değeri 3 olan araçlar");
 			foreach (var cars in carManager.GetCarsByBrandId(3))
 			{
@@ -50,6 +58,7 @@ namespace ConsoleUI
 			}
 
 			Console.WriteLine("--------------------");
+			//ColorId değeri 3 olan araçları getirdik
 			Console.WriteLine("ColorId değeri 1 olan araçlar");
 			foreach (var cars in carManager.GetCarsByColorId(1))
 			{
